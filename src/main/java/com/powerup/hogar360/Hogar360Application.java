e0b6d7cd0c2564aa9a49ed57407bc8e5f7d1c645
Edgar J. Salas <ejsalasdev@gmail.com>
2025-03-07 19:52:14 -0500

    Se inicializa proyecto base

---
+++ src/main/java/com/powerup/hogar360/Hogar360Application.java
@@ -0,0 +1,13 @@
+package com.powerup.hogar360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hogar360Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hogar360Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/powerup/hogar360/Hogar360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powerup.hogar360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hogar360ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
